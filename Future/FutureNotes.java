6159ddd1432f1188288740f75b5599a9a39fec75
HOME <HOME@gopal>
2020-06-21 14:42:45 -0700

    Future Notes

--- Future/FutureNotes.java
+++ Future/FutureNotes.java
@@ -26,3 +26,37 @@
 		 Thread d=threadFactory.newThread(new Task());
 		 d.start();
 	}
+	
+	
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+5) 
+All you have to do is maintain a list of Futures that you submit, and then you can iterate over the whole list of Futures and either:
+  A) wait for all the futures to be done in a blocking way or
+  B) check if all the futures are done in a non-blocking way.
+
+Here is a code example:
+
+List<Future<?>> futures = new ArrayList<Future<?>>();
+ExecutorService exec = Executors.newFixedThreadPool(5);
+
+// Instead of using exec.execute() use exec.submit()
+// because it returns a monitorable future
+while((item = stack.pollFirst()) != null){
+    Runnable worker = new Solider(this, item);
+    Future<?> f = exec.submit(worker);
+    futures.add(f);
+}
+
+// A) Await all runnables to be done (blocking)
+for(Future<?> future : futures)
+    future.get(); // get will block until the future is done
+
+// B) Check if all runnables are done (non-blocking)
+boolean allDone = true;
+for(Future<?> future : futures){
+    allDone &= future.isDone(); // check if future is done
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+6)
