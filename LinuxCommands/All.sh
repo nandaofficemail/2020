f7484e593c068638ee05640161ee5e118ce5eaf1
nandajavadocs <nandaofficemail@gmail.com>
2020-09-05 13:08:47 +0530

    Update All.sh

--- LinuxCommands/All.sh
+++ LinuxCommands/All.sh
@@ -17,6 +17,9 @@ mount 10.10.0.10:/backups
 S:
 ----------
 sudo apt update
+sudo apt update
+sudo apt install cifs-utils
+sudo dnf install cifs-utils
 sudo apt install nfs-common
 sudo mkdir /var/backups
 sudo mount -t nfs 10.10.0.10:/backups /var/backups
@@ -24,6 +27,13 @@ sudo nano /etc/fstab
 sudo mkdir /var/backups
 sudo mkdir /mnt/win_share
 sudo nano /etc/fstab
+sudo mount -t cifs -o username=<win_share_user> //WIN_SHARE_IP/<share_name> /mnt/win_share
+sudo mount -t cifs -o username=<win_share_user>,password=<win_share_password> //WIN_SHARE_IP/<share_name> /mnt/win_share
+sudo mount -t cifs -o username=<win_share_user>,domain=<win_domain> //WIN_SHARE_IP/<share_name> /mnt/win_share
+sudo chown root: /etc/win-credentials
+sudo chmod 600 /etc/win-credentials
+sudo mount -t cifs -o credentials=/etc/win-credentials //WIN_SHARE_IP/<share_name> /mnt/win_share
+sudo umount /mnt/win_share
 
 U
 ----
