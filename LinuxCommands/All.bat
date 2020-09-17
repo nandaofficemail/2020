4fabaceb2cd73cbdddaff67307216d053149c54d
nandajavadocs <nandaofficemail@gmail.com>
2020-09-17 16:43:32 +0530

    Update All.bat

--- LinuxCommands/All.bat
+++ LinuxCommands/All.bat
@@ -12,3 +12,19 @@ inetcpl.cpl
 
 nslookup www.google.com
 
+netstat -ano | find "80"
+netstat -anb -p tcp | find "80"
+taskkill /PID 4 /F
+
+To Disable port 80
+------------------
+
+1) net stop http
+
+OR
+
+2) Device Manager -> Show Hidden Devices -> plug and play -> Http -> Stop
+
+C:\windows\System32\drivers\Http.sys
+
+msconfig | compmgmt | Device Manager
